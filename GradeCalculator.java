b8a4dac3ba34de042f2203193e44ffec055fd99c
shrek-wtih-no-r <cmsarr100@student.hbuhsd.edu>
2024-09-13 11:01:08 -0700

    +

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,29 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String courseName = ("Linear Algebra");
+    int averageMinutes = 10068;
+    int firstHwrade = 96;
+    int secondHwGrade = 94;
+    int thirdHwGrade = 99;
+    int fourthGHwrade = 91;
+    double quiz1 = 100;
+    double quiz2 = 99.9;
+    
+    double hwAvg = ((firstHwrade + secondHwGrade + thirdHwGrade + fourthGHwrade) / 4);
+    double quizAvg = ((quiz1 + quiz2) / 2);
+    double finalExam = 0.1;
+
+    double finalGrade = ((int)((hwAvg * 35) + (quizAvg * 10) + (finalExam * 50)) / 100);
+    
+    System.out.println("Course Name: " + courseName);
+    System.out.println("Average homework grade: " + hwAvg);
+    System.out.println("Average Quiz grade: " + quizAvg);
+    System.out.println("overall grade: " + finalGrade);
+    
+    if (finalGrade < 50)
+    {
+      System.out.println("how tf did you spend " + averageMinutes + " minutes and still fail");
+    }
   }
 }
